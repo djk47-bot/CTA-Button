a5e0656889e573bc38682ab6f636f948beeb0be5
Justin Kelly <justink124@outlook.com>
2021-09-20 22:16:33 -0400

    updated btn src, included team notes

--- Group19j/src/CtaBtn.js
+++ Group19j/src/CtaBtn.js
@@ -5,8 +5,24 @@ export class CtaBtn extends LitElement {
     return css`
       :host {
         display: block;
-        padding: 25px;
+        padding: 20px;
         color: var(--cta-btn-text-color, #000);
+      }
+      a {
+        border-radius: 10px;
+        border-width: 1px;
+        text-decoration: none;
+        background-color: lightgray;
+        padding: 10px 12px 10px 12px;
+        font-size: 15px;
+        color: black;
+        transition: 0.2s;
+      }
+      a:hover,
+      a:focus {
+        color: white;
+        background-color: darkgray;
+        transition: 0.2s;
       }
     `;
   }
@@ -28,10 +44,16 @@ export class CtaBtn extends LitElement {
     this.counter += 1;
   }
 
+  /* TODO
+    Styling
+    Accessibility tabbing
+    href link
+    update @click functionality
+  */
   render() {
     return html`
-      <h2>${this.title} Nr. ${this.counter}!</h2>
-      <button @click=${this.__increment}>increment</button>
+      <h2>${this.title} Counter: ${this.counter}!</h2>
+      <a role="button" href="${this.link}" @click=${this.__increment}>increment</button>
     `;
   }
 }
