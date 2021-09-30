43f236930dd8513c20684ad10f351753969951c4
Joseph.TJK <t.joseph.tjk@gmail.com>
2021-09-30 14:26:55 -0400

    update

--- j-btn/src/JBtn.js
+++ j-btn/src/JBtn.js
@@ -10,7 +10,13 @@ export class JBtn extends LitElement {
         padding: 25px;
         color: var(--j-btn-text-color, darkorange);
       }
-
+      :host([disabled]) h2 {
+        background-color: blue;
+      }
+      a {
+        display: inline-block;
+        color: #333333;
+      }
       .j-btn {
         box-shadow: 0px 1px 0px 0px #fff6af;
         background: linear-gradient(to bottom, #ffec64 5%, #ffab23 100%);
@@ -41,18 +47,23 @@ export class JBtn extends LitElement {
       h2 {
         font-color: white;
       }
+      simple-icon-lite {
+        color: orange;
+      }
     `;
   }
 
   static get properties() {
     return {
       title: { type: String },
+      disabled: { type: Boolean, reflect: true },
     };
   }
 
   constructor() {
     super();
     this.title = 'Hey there';
+    this.disabled = false;
     this.link =
       'https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjj3qaDoqXzAhWwTt8KHbT5DMgQyCl6BAgFEAM&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3Druwu3RKnugk&usg=AOvVaw2QR_vGXqHsGSCTk_zt1zk_';
   }
@@ -60,14 +71,9 @@ export class JBtn extends LitElement {
   render() {
     return html`
       <h2>${this.title}</h2>
-      <a
-        tabindex="-1"
-        rel="noopener"
-        target="_blank"
-        href="${this.link}"
-        class="j-btn"
-        >Click if you dare
-        <button>
+      <a tabindex="-1" rel="noopener" target="_blank" href="${this.link}">
+        <button class="j-btn" ?disabled="${this.disabled}">
+          Click if you dare
           <simple-icon-lite icon="block"></simple-icon-lite>
         </button>
       </a>
